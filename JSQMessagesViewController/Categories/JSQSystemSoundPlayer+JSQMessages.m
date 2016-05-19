680310bbcb938eba1b91ae07a508907d2d30f980
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-05-18 20:50:30 -0500

    working on Carthage integration

--- JSQMessagesViewController/Categories/JSQSystemSoundPlayer+JSQMessages.m
+++ JSQMessagesViewController/Categories/JSQSystemSoundPlayer+JSQMessages.m
@@ -16,8 +16,7 @@
 //  Released under an MIT license: http://opensource.org/licenses/MIT
 //
 
-@import JSQSystemSoundPlayer;
-//#import "JSQSystemSoundPlayer+JSQMessages.h"
+#import "JSQSystemSoundPlayer+JSQMessages.h"
 
 static NSString * const kJSQMessageReceivedSoundName = @"message_received";
 static NSString * const kJSQMessageSentSoundName = @"message_sent";
